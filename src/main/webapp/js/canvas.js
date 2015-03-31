1abe19c85ecb2ed8bbdd654a5a65611224703154
Piyush Mishra <piyushmishra889@gmail.com>
2015-03-31 13:22:43 +0530

    added libray function to draw a ball

--- src/main/webapp/js/canvas.js
+++ src/main/webapp/js/canvas.js
@@ -2,22 +2,27 @@
  * script to be operated on canvas
  */
 
-// get element by id
-function getElement(elementId) {
-	return document.getElementById(elementId)
-}
-
-// set image in canvas
-function setImageInCanvas(canvas) {
-    var canvasContext = canvas.getContext("2d")
-	canvasContext.beginPath()
-    canvasContext.arc(350,480,10,Math.PI*2,false)
-    canvasContext.closePath()
-    canvasContext.fill()
-}
+var ball_x = 350
+var ball_y = 480
+var ball_x_displacement = 2
+var ball_y_displacement = 2
+var canvasId = "canvas"
+var radius = 10
 
 // draw image
 function drawImage(canvasId) {
-	var canvas = getElement(canvasId)
-    setImageInCanvas(canvas)
+	var ball = new Ball(ball_x, ball_y, radius)
+}
+
+
+f
+
+// create the ball class
+function Ball(ball_x_position, ball_y_position, radius) {
+	var canvas = document.getElementById("canvas")
+	var ctx = canvas.getContext("2d")
+	ctx.beginPath()
+	ctx.arc(ball_x_position, ball_y_position, radius, Math.PI * 2, false)
+	ctx.closePath()
+	ctx.fill()
 }
