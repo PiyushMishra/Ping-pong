113ce5268220c55bc49c1a6f24a995b401be8526
Piyush Mishra <piyushmishra889@gmail.com>
2015-03-31 13:47:21 +0530

    create multiple images

--- src/main/webapp/js/canvas.js
+++ src/main/webapp/js/canvas.js
@@ -4,22 +4,30 @@
 
 var ball_x = 350
 var ball_y = 480
-var ball_x_displacement = 2
-var ball_y_displacement = 2
+var ball_x_displacement = 20
+var ball_y_displacement = 20
 var canvasId = "canvas"
 var radius = 10
 
+
 // draw image
-function drawImage(canvasId) {
-	var ball = new Ball(ball_x, ball_y, radius)
+function drawImage(ball_x_position, ball_y_position) {
+    v
+	var ball = new Ball(ball_x_position, ball_y_position, radius)
+	
+}
+
+//loop draw
+function drawImages() {
+for (var i = 0; i < 10; i++) {
+	var ball = new Ball(ball_x + ball_x_displacement*i, ball_y - ball_y_displacement*i,10)
+}
 }
 
 
-f
-
 // create the ball class
 function Ball(ball_x_position, ball_y_position, radius) {
-	var canvas = document.getElementById("canvas")
+	var canvas = document.getElementById(canvasId)
 	var ctx = canvas.getContext("2d")
 	ctx.beginPath()
 	ctx.arc(ball_x_position, ball_y_position, radius, Math.PI * 2, false)
